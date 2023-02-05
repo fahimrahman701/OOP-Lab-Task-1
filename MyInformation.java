59898d7dfa5186014538d33d0f542655ee94db67
Md. Fahimur Rahman <110930113+fahimrahman701@users.noreply.github.com>
2023-02-05 16:50:31 +0600

    Add files via upload

---
+++ MyInformation.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myinformation;
+
+/**
+ *
+ * @author User
+ */
+public class MyInformation {
+
+    public static void main(String[] args) {
+        System.out.println("Name : Md. Fahimur Rahman");
+        System.out.println("Id : 221-15-5953");
+        System .out .println("Department : Computer Science And Engineering");
+        System.out.println("Address : Daffodil International University,Ashulia,Savar");
+        System.out.println("Favorite Game : Carrom, Badminton, Cricket, Faootball");
+    }
+}
