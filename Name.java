1027d53b8b1991a39b9fe16afd1c8bc08536e158
Md. Fahimur Rahman <110930113+fahimrahman701@users.noreply.github.com>
2023-02-05 17:13:33 +0600

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.name;
+
+/**
+ *
+ * @author User
+ */
+public class Name {
+
+    public static void main(String[] args) {
+        String name = "Md. Fahimur Rahman";
+        System.out.println("My name is " + name);
+    }
+}
